958690275e817c3a387af69807bb2e8b863ebfd2
Simon1616 <scastle5566@student.pps.net>
2024-10-08 13:56:43 -0700

    fixed stuff, broke even more stuff

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public Vector3 target;
+    public Vector3 direction;
+    public int speed = 10;
+    public float lifespan = 120;
+    Turret Pos;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Pos = FindObjectOfType<Turret>();
+        target = Pos.targetPos;
+        direction = new Vector3(transform.position.x - target.x, transform.position.y - target.y, 0).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifespan -= Time.deltaTime;
+        if (lifespan <= 0)
+        {
+            Destroy(gameObject);
+        }
+        direction = direction * speed;
+        transform.Translate(direction * Time.deltaTime);
     }
 }
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,33 +1,40 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    private int firerate;
+    private float firerate;
+    private float cooldown;
+    public Vector3 targetPos;
 
-    public GameObject projectileClone; 
+    public GameObject projectilePrefab;
+    
     public Vector3 spawnPos;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         firerate = 2;
+        cooldown = 1;
+        
         spawnPos = new Vector3(transform.position.x, transform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cooldown -= Time.deltaTime;
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && cooldown <= 0)
         {
-            GameObject clone = Instantiate(projectileClone, spawnPos, Quaternion.identity);
-            ProjectileScript cloneScript = projectileClone.GetComponent<ProjectileScript>();
-            
+            Debug.Log("shouldshoot");
+            Instantiate(projectilePrefab, spawnPos, quaternion.identity);
+            targetPos = other.gameObject.transform.position;
+            cooldown = firerate;
         }
     }
 }
