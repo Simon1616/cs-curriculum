fd75613d777ea3741b4b114481de75ae884c2e77
Simon1616 <scastle5566@student.pps.net>
2024-10-16 11:40:30 -0700

    stuff that will work maybe?

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,15 +1,57 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum states
+    {
+        patrolling,
+        chasing,
+        attacking,
+        die
+    }
+
+    private states state;
+    int speed = 4;
+    private Vector3 target;
+
+    public PlayerController playerController;
+
+    private void Start()
+    {
+        state = states.patrolling;
+    }
+
+    private void Update()
+    {
+        target = new Vector3(playerController.playerX, playerController.playerY, 0);
+        if (state == states.chasing)
+        {
+            Chase();
+        }
+        else if (state == states.attacking)
+        {
+            Attack();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+        
+    }
+
+    void Chase()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attack()
     {
         
     }
