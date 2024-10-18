cc2454222caedb6ad0eed1c643df86e1cfb4b143
Simon1616 <scastle5566@student.pps.net>
2024-10-18 15:10:28 -0700

    Enemy Moves

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
@@ -12,21 +13,29 @@ public class EnemyScript : MonoBehaviour
         attacking,
         die
     }
+    
 
     private states state;
-    int speed = 4;
+    
+    float speed = 2;
+    private float duration = 5;
+    
     private Vector3 target;
+    
+    private GameObject player = null;
 
     public PlayerController playerController;
+
+    private Vector3 direction;
 
     private void Start()
     {
         state = states.patrolling;
+        playerController = FindFirstObjectByType<PlayerController>();
     }
 
     private void Update()
     {
-        target = new Vector3(playerController.playerX, playerController.playerY, 0);
         if (state == states.chasing)
         {
             Chase();
@@ -35,24 +44,64 @@ public class EnemyScript : MonoBehaviour
         {
             Attack();
         }
+        else if (state == states.patrolling)
+        {
+            Patrol();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (player == null)
+            {
+                state = states.chasing;
+            }
+
+            if (player != null)
+            {
+                state = states.attacking;
+            }
+            player = other.gameObject;
             
         }
         
     }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (state == states.attacking)
+            {
+                state = states.chasing;
+            }
+            else if (state == states.chasing)
+            {
+                state = states.patrolling;
+                player = null;
+            }
+        }
+    }
+
     void Chase()
     {
-        
+        target = new Vector3(playerController.playerX, playerController.playerY, 0);
+        direction = ((target - transform.position).normalized) * speed;
+        transform.position += (direction * Time.deltaTime);
     }
 
     void Attack()
     {
-        
+        Debug.Log("attacked");
+    }
+
+    void Patrol()
+    {
+        while (true)
+        {
+            
+        }
     }
 }
